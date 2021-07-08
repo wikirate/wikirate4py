02de347d5ec0b02b88b912958fc2c7cf17af5efb
vasgat <vasgat@iti.gr>
2021-07-08 17:19:38 +0300

    minor edits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 
 import wikirate4py
+import html2text
 
 # -- Project information -----------------------------------------------------
 
