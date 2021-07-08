6344b5e5133ed5579981f2fd8766a198a03a0fde
vasgat <vasgat@iti.gr>
2021-07-08 17:05:21 +0300

    minor edit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,6 @@ author = 'Vasiliki Gkatziaki'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.intersphinx',
